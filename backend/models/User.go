cf142199317207f1a5f3cc82e84e0c9351fc91a0
NeelMalwatkar <nmalwatkar@ufl.edu>
2025-01-31 00:18:53 -0500

    added new parameters for user registration

--- backend/models/User.go
+++ backend/models/User.go
@@ -2,6 +2,9 @@ package models
 
 type User struct {
 	ID       uint   `gorm:"column:id;primaryKey"`
+	Fullname string `gorm:"column:fullname;not null"`
+	Location string `gorm:"column:location;not null"`
+	DOB      string `gorm:"column:dob;not null"` //DOB from ISO 8601 format yyy-mm-dd
 	Username string `gorm:"column:username;unique;not null"`
 	Email    string `gorm:"column:email;unique;not null"`
 	Password string `gorm:"column:password;not null"`
