fe6e28494c44ee8da1f8a6029dcaafefa11f5394
NeelMalwatkar <nmalwatkar@ufl.edu>
2025-02-07 20:34:56 -0500

    added env vars for db path.

--- backend/api/connectToDatabase.go
+++ backend/api/connectToDatabase.go
@@ -1,12 +1,27 @@
 package api
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func databaseConnection() (*gorm.DB, error) {
-	var pathToDatabase string = "../database/roamioDb.db"
+	var pathToDatabase string
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file") // Log error but continue if already loaded
+	}
+
+	// Get the database path from the environment variable
+	pathToDatabase = os.Getenv("DATABASE_PATH")
+	if pathToDatabase == "" {
+		pathToDatabase = "../database/roamioDb.db" // Default path if not set
+	}
+
 	database, err := gorm.Open(sqlite.Open(pathToDatabase), &gorm.Config{})
 	if err != nil {
 		return nil, err
