62ad4177d23127e494e8dbf1ca71d94cb6b306df
riya1315 <riddhimehta1511@gmail.com>
2025-02-28 15:13:39 -0500

    latest sync

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-// import {BrowserRouter, Routes, Route} from "react-router-dom";
-// import Login from './components';
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
-
 import LandingPage from "./components/LandingPage";
 
 function App() {
