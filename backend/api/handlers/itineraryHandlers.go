14a369df8f3321d1b6f682e39de975ebe22501af
NeelMalwatkar <nmalwatkar@ufl.edu>
2025-03-28 01:08:31 -0400

    changed minimum values for nights and days

--- backend/api/handlers/itineraryHandlers.go
+++ backend/api/handlers/itineraryHandlers.go
@@ -65,7 +65,7 @@ func CreateItinerary(c *gin.Context) {
 		return
 	}
 
-	if itinerary.Title == "" || itinerary.UserID == 0 || itinerary.StateId == 0 || itinerary.NumDays == 0 || itinerary.NumNights == 0 || itinerary.Size == 0 || itinerary.Budget == "" {
+	if itinerary.Title == "" || itinerary.UserID == 0 || itinerary.StateId == 0 || itinerary.Size == 0 || itinerary.Budget == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing required fields"})
 		return
 	}
