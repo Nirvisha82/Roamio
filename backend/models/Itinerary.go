9f96dfbafca4312fc5d2cd04f5a948af32935c02
NeelMalwatkar <nmalwatkar@ufl.edu>
2025-03-28 01:07:16 -0400

    added created and updated timestamps to the itinerary table

--- backend/models/Itinerary.go
+++ backend/models/Itinerary.go
@@ -1,16 +1,20 @@
 package models
 
+import "time"
+
 type Itinerary struct {
-	ID          uint   `gorm:"column:id;primaryKey"`
-	UserID      uint   `gorm:"column:userID;not null"`
-	StateId     uint   `gorm:"column:stateID;not null"`
-	Title       string `gorm:"column:title;not null"`
-	Description string `gorm:"column:description;not null"`
-	NumDays     uint   `gorm:"column:numdays;not null"`
-	NumNights   uint   `gorm:"column:numnights;not null"`
-	Size        uint   `gorm:"column:size;not null"`
-	Budget      string `gorm:"column:budget;not null"`
-	Highlights  string `gorm:"column:highlights"`
-	Suggestions string `gorm:"column:suggestions"`
-	Images      string `gorm:"column:images"`
+	ID          uint      `gorm:"column:id;primaryKey"`
+	UserID      uint      `gorm:"column:userID;not null"`
+	StateId     uint      `gorm:"column:stateID;not null"`
+	Title       string    `gorm:"column:title;not null"`
+	Description string    `gorm:"column:description;not null"`
+	NumDays     uint      `gorm:"column:numdays;not null"`
+	NumNights   uint      `gorm:"column:numnights;not null"`
+	Size        uint      `gorm:"column:size;not null"`
+	Budget      string    `gorm:"column:budget;not null"`
+	Highlights  string    `gorm:"column:highlights"`
+	Suggestions string    `gorm:"column:suggestions"`
+	Images      string    `gorm:"column:images"`
+	PostedAt    time.Time `gorm:"column:posted_at;autoCreateTime"`
+	UpdatedAt   time.Time `gorm:"column:updated_at;autoUpdateTime"`
 }
