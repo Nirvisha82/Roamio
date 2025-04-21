b360d3bc2df9c42eb86b403171d0188c05773a1b
HarshKh13 <harshkhanna1309@gmail.com>
2025-04-20 23:29:05 -0400

    Service to fetch top K users by followers

--- backend/api/services/itinerary_services.go
+++ backend/api/services/itinerary_services.go
@@ -27,3 +27,26 @@ func TopKStatesByFollowers(database *gorm.DB, k int) ([]struct {
 
 	return results, err
 }
+
+func TopKUserByFollowers(database *gorm.DB, k int) ([]struct {
+	UserID        uint   `json:"user_id"`
+	UserName      string `json:"username"`
+	FollowerCount int    `json:"follower_count"`
+}, error) {
+	var results []struct {
+		UserID        uint   `json:"user_id"`
+		UserName      string `json:"username"`
+		FollowerCount int    `json:"follower_count"`
+	}
+
+	err := database.Table("follows").
+		Select("users.id as user_id, users.username as username, COUNT(follows.id) as follower_count").
+		Joins("JOIN users ON users.id = follows.target_id").
+		Where("follows.type = ?", "user").
+		Group("users.id").
+		Order("follower_count DESC").
+		Limit(k).
+		Scan(&results).Error
+
+	return results, err
+}
